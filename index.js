99a213ac45ac4fa429ad670b0976a94b92ab464a
unknown <finako243@gmail.com>
2024-04-07 11:51:48 +0300

    New Tresor

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello .NET')
+import { useState } from 'react';
+
+function SearchableVideoList({ videos }) {
+  const [searchText, setSearchText] = useState('');
+  const foundVideos = filterVideos(videos, searchText);
+  return (
+    <>
+      <SearchInput
+        value={searchText}
+        onChange={newText => setSearchText(newText)} />
+      <VideoList
+        videos={foundVideos}
+        emptyHeading={`No matches for “${searchText}”`} />
+    </>
+  );
+}
