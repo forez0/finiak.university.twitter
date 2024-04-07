62ccc849b5a43f936cdbb7d4b26630222c63e982
unknown <finako243@gmail.com>
2024-04-07 21:03:33 +0300

    Design

--- tresor/src/App.js
+++ tresor/src/App.js
@@ -2,17 +2,40 @@ import logo from './TresorLogo.png';
 import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode"
-
+import {useGoogleLogin} from '@react-oauth/google';
+import axios from "axios"
 function App() {
+  const login = useGoogleLogin({
+    onSuccess: async respose => {
+        try {
+            const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+                headers: {
+                    "Authorization": `Bearer ${respose.access_token}`
+                }
+            })
+
+            console.log(res.data)
+        } catch (err) {
+            console.log(err)
+
+        }
+
+    }
+});
   return (
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="Text">
           Tresor
         </p>
-        <span>
-        <GoogleLogin
+
+        <button onClick={login}>
+                    <i class="fa-brands fa-google"></i>
+                    Continue with google
+                </button>
+        {/* <GoogleLogin className="Google"
   onSuccess={credentialResponse => {
     const decoded = jwtDecode(credentialResponse.credential);
     console.log(decoded);
@@ -20,9 +43,7 @@ function App() {
   onError={() => {
     console.log('Login Failed');
   }}
-  auto_select
-/>
-</span> 
+/> */}
       </header>
     </div>
   )
