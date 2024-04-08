cd7571d5161489dac8c0c6ee44a41ffdc2f67ed8
unknown <finako243@gmail.com>
2024-04-08 17:33:01 +0300

    Routes

--- tresor/src/App.js
+++ tresor/src/App.js
@@ -1,54 +1,18 @@
-import logo from './TresorLogo.png';
-import './App.css';
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode"
-import  google  from './misc/Google_icon.png'
-import {useGoogleLogin} from '@react-oauth/google';
-import axios from "axios"
+import React from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./Login"
+
 function App() {
-  const login = useGoogleLogin({
-    onSuccess: async respose => {
-        try {
-            const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-                headers: {
-                    "Authorization": `Bearer ${respose.access_token}`
-                }
-            })
-
-            console.log(res.data)
-        } catch (err) {
-            console.log(err)
-
-        }
-
-    }
-});
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="Text">
-          Tresor
-        </p>
-
-        <button onClick={login} className="row-button">
-        <img src={google} className="Google-logo" alt="Google Logo" />
-                    <i class="fa-brands fa-google"></i>
-                    Continue with Google
-                </button>
-        {/* <GoogleLogin className="Google"
-  onSuccess={credentialResponse => {
-    const decoded = jwtDecode(credentialResponse.credential);
-    console.log(decoded);
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/> */}
-      </header>
-    </div>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Navigate to="/login" />}/>
+                    <Route path="/Login" element={<Login />}/>
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
---
+++ tresor/src/Login/index.jsx
@@ -0,0 +1,52 @@
+import logo from '../TresorLogo.png';
+import React from "react";
+import './Login.css';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode"
+import  google  from '../misc/Google_icon.png'
+import {useGoogleLogin} from '@react-oauth/google';
+import axios from "axios"
+
+function Login() {
+  const login = useGoogleLogin({
+    onSuccess: async respose => {
+        try {
+            const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+                headers: {
+                    "Authorization": `Bearer ${respose.access_token}`
+                }
+            })
+            console.log(res.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+  });
+  return (
+    <div className="Login">
+      <header className="Login-header">
+        <img src={logo} className="Login-logo" alt="logo" />
+        <p className="text">
+          Tresor
+        </p>
+
+        <button onClick={login} className="row-button">
+        <img src={google} className="Google-logo" alt="Google Logo" />
+                    <i class="fa-brands fa-google"></i>
+                    Continue with Google
+                </button>
+        {/* <GoogleLogin className="Google"
+  onSuccess={credentialResponse => {
+    const decoded = jwtDecode(credentialResponse.credential);
+    console.log(decoded);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/> */}
+      </header>
+    </div>
+  )
+}
+
+export default Login;
